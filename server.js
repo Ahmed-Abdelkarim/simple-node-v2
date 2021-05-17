dbf2cebb67ed315a0f4364cf2098c5172e9f1010
Ahmed-Abdelkarim <ahmedabdelhafez20111@gmail.com>
2021-05-17 16:52:24 +0200

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
+const favouriteFood = process.env.FAVOURITE_FOOD;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    const favouriteFood = process.env.FAVOURITE_FOOD;
     console.log(`My favourite food is ${favouriteFood}`);	  
     await sleep(5000);
   }
